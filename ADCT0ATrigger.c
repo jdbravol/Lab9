b411406bcdec8ced67184304ed1ad778bf0cbd45
root <curewitzjustin@gmail.com>
2016-11-01 18:27:14 -0500

    added all three initializations

--- ADCT0ATrigger.c
+++ ADCT0ATrigger.c
@@ -22,7 +22,7 @@
  http://users.ece.utexas.edu/~valvano/
  */
 #include <stdint.h>
-#include "../inc/tm4c123gh6pm.h"
+#include "tm4c123gh6pm.h"
 #define NVIC_EN0_INT17          0x00020000  // Interrupt 17 enable
 
 #define TIMER_CFG_16_BIT        0x00000004  // 16-bit timer configuration,
@@ -103,127 +103,128 @@ void WaitForInterrupt(void);  // low power mode
 // SS3 triggering event: Timer0A
 // SS3 1st sample source: programmable using variable 'channelNum' [0:11]
 // SS3 interrupts: enabled and promoted to controller
-void ADC0_InitTimer0ATriggerSeq3(uint8_t channelNum, uint32_t period){
-  volatile uint32_t delay;
-  // **** GPIO pin initialization ****
-  switch(channelNum){             // 1) activate clock
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-    case 8:
-    case 9:                       //    these are on GPIO_PORTE
-      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4; break;
-    case 4:
-    case 5:
-    case 6:
-    case 7:                       //    these are on GPIO_PORTD
-      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3; break;
-    case 10:
-    case 11:                      //    these are on GPIO_PORTB
-      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1; break;
-    default: return;              //    0 to 11 are valid channels on the LM4F120
-  }
-  delay = SYSCTL_RCGCGPIO_R;      // 2) allow time for clock to stabilize
-  delay = SYSCTL_RCGCGPIO_R;
-  switch(channelNum){
-    case 0:                       //      Ain0 is on PE3
-      GPIO_PORTE_DIR_R &= ~0x08;  // 3.0) make PE3 input
-      GPIO_PORTE_AFSEL_R |= 0x08; // 4.0) enable alternate function on PE3
-      GPIO_PORTE_DEN_R &= ~0x08;  // 5.0) disable digital I/O on PE3
-      GPIO_PORTE_AMSEL_R |= 0x08; // 6.0) enable analog functionality on PE3
-      break;
-    case 1:                       //      Ain1 is on PE2
-      GPIO_PORTE_DIR_R &= ~0x04;  // 3.1) make PE2 input
-      GPIO_PORTE_AFSEL_R |= 0x04; // 4.1) enable alternate function on PE2
-      GPIO_PORTE_DEN_R &= ~0x04;  // 5.1) disable digital I/O on PE2
-      GPIO_PORTE_AMSEL_R |= 0x04; // 6.1) enable analog functionality on PE2
-      break;
-    case 2:                       //      Ain2 is on PE1
-      GPIO_PORTE_DIR_R &= ~0x02;  // 3.2) make PE1 input
-      GPIO_PORTE_AFSEL_R |= 0x02; // 4.2) enable alternate function on PE1
-      GPIO_PORTE_DEN_R &= ~0x02;  // 5.2) disable digital I/O on PE1
-      GPIO_PORTE_AMSEL_R |= 0x02; // 6.2) enable analog functionality on PE1
-      break;
-    case 3:                       //      Ain3 is on PE0
-      GPIO_PORTE_DIR_R &= ~0x01;  // 3.3) make PE0 input
-      GPIO_PORTE_AFSEL_R |= 0x01; // 4.3) enable alternate function on PE0
-      GPIO_PORTE_DEN_R &= ~0x01;  // 5.3) disable digital I/O on PE0
-      GPIO_PORTE_AMSEL_R |= 0x01; // 6.3) enable analog functionality on PE0
-      break;
-    case 4:                       //      Ain4 is on PD3
-      GPIO_PORTD_DIR_R &= ~0x08;  // 3.4) make PD3 input
-      GPIO_PORTD_AFSEL_R |= 0x08; // 4.4) enable alternate function on PD3
-      GPIO_PORTD_DEN_R &= ~0x08;  // 5.4) disable digital I/O on PD3
-      GPIO_PORTD_AMSEL_R |= 0x08; // 6.4) enable analog functionality on PD3
-      break;
-    case 5:                       //      Ain5 is on PD2
-      GPIO_PORTD_DIR_R &= ~0x04;  // 3.5) make PD2 input
-      GPIO_PORTD_AFSEL_R |= 0x04; // 4.5) enable alternate function on PD2
-      GPIO_PORTD_DEN_R &= ~0x04;  // 5.5) disable digital I/O on PD2
-      GPIO_PORTD_AMSEL_R |= 0x04; // 6.5) enable analog functionality on PD2
-      break;
-    case 6:                       //      Ain6 is on PD1
-      GPIO_PORTD_DIR_R &= ~0x02;  // 3.6) make PD1 input
-      GPIO_PORTD_AFSEL_R |= 0x02; // 4.6) enable alternate function on PD1
-      GPIO_PORTD_DEN_R &= ~0x02;  // 5.6) disable digital I/O on PD1
-      GPIO_PORTD_AMSEL_R |= 0x02; // 6.6) enable analog functionality on PD1
-      break;
-    case 7:                       //      Ain7 is on PD0
-      GPIO_PORTD_DIR_R &= ~0x01;  // 3.7) make PD0 input
-      GPIO_PORTD_AFSEL_R |= 0x01; // 4.7) enable alternate function on PD0
-      GPIO_PORTD_DEN_R &= ~0x01;  // 5.7) disable digital I/O on PD0
-      GPIO_PORTD_AMSEL_R |= 0x01; // 6.7) enable analog functionality on PD0
-      break;
-    case 8:                       //      Ain8 is on PE5
-      GPIO_PORTE_DIR_R &= ~0x20;  // 3.8) make PE5 input
-      GPIO_PORTE_AFSEL_R |= 0x20; // 4.8) enable alternate function on PE5
-      GPIO_PORTE_DEN_R &= ~0x20;  // 5.8) disable digital I/O on PE5
-      GPIO_PORTE_AMSEL_R |= 0x20; // 6.8) enable analog functionality on PE5
-      break;
-    case 9:                       //      Ain9 is on PE4
-      GPIO_PORTE_DIR_R &= ~0x10;  // 3.9) make PE4 input
-      GPIO_PORTE_AFSEL_R |= 0x10; // 4.9) enable alternate function on PE4
-      GPIO_PORTE_DEN_R &= ~0x10;  // 5.9) disable digital I/O on PE4
-      GPIO_PORTE_AMSEL_R |= 0x10; // 6.9) enable analog functionality on PE4
-      break;
-    case 10:                      //       Ain10 is on PB4
-      GPIO_PORTB_DIR_R &= ~0x10;  // 3.10) make PB4 input
-      GPIO_PORTB_AFSEL_R |= 0x10; // 4.10) enable alternate function on PB4
-      GPIO_PORTB_DEN_R &= ~0x10;  // 5.10) disable digital I/O on PB4
-      GPIO_PORTB_AMSEL_R |= 0x10; // 6.10) enable analog functionality on PB4
-      break;
-    case 11:                      //       Ain11 is on PB5
-      GPIO_PORTB_DIR_R &= ~0x20;  // 3.11) make PB5 input
-      GPIO_PORTB_AFSEL_R |= 0x20; // 4.11) enable alternate function on PB5
-      GPIO_PORTB_DEN_R &= ~0x20;  // 5.11) disable digital I/O on PB5
-      GPIO_PORTB_AMSEL_R |= 0x20; // 6.11) enable analog functionality on PB5
-      break;
-  }
-  DisableInterrupts();
-  SYSCTL_RCGCADC_R |= 0x01;     // activate ADC0 
-  SYSCTL_RCGCTIMER_R |= 0x01;   // activate timer0 
-  delay = SYSCTL_RCGCTIMER_R;   // allow time to finish activating
-  TIMER0_CTL_R = 0x00000000;    // disable timer0A during setup
-  TIMER0_CTL_R |= 0x00000020;   // enable timer0A trigger to ADC
-  TIMER0_CFG_R = 0;             // configure for 32-bit timer mode
-  TIMER0_TAMR_R = 0x00000002;   // configure for periodic mode, default down-count settings
-  TIMER0_TAPR_R = 0;            // prescale value for trigger
-  TIMER0_TAILR_R = period-1;    // start value for trigger
-  TIMER0_IMR_R = 0x00000000;    // disable all interrupts
-  TIMER0_CTL_R |= 0x00000001;   // enable timer0A 32-b, periodic, no interrupts
-  ADC0_PC_R = 0x01;         // configure for 125K samples/sec
-  ADC0_SSPRI_R = 0x3210;    // sequencer 0 is highest, sequencer 3 is lowest
-  ADC0_ACTSS_R &= ~0x08;    // disable sample sequencer 3
-  ADC0_EMUX_R = (ADC0_EMUX_R&0xFFFF0FFF)+0x5000; // timer trigger event
-  ADC0_SSMUX3_R = channelNum;
-  ADC0_SSCTL3_R = 0x06;          // set flag and end                       
-  ADC0_IM_R |= 0x08;             // enable SS3 interrupts
-  ADC0_ACTSS_R |= 0x08;          // enable sample sequencer 3
-  NVIC_PRI4_R = (NVIC_PRI4_R&0xFFFF00FF)|0x00004000; //priority 2
-  NVIC_EN0_R = 1<<17;              // enable interrupt 17 in NVIC
+//void ADC0_InitTimer0ATriggerSeq3(uint8_t channelNum, uint32_t period){
+//  volatile uint32_t delay;
+//  // **** GPIO pin initialization ****
+//  switch(channelNum){             // 1) activate clock
+//    case 0:
+//    case 1:
+//    case 2:
+//    case 3:
+//    case 8:
+//    case 9:                       //    these are on GPIO_PORTE
+//      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4; break;
+//    case 4:
+//    case 5:
+//    case 6:
+//    case 7:                       //    these are on GPIO_PORTD
+//      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3; break;
+//    case 10:
+//    case 11:                      //    these are on GPIO_PORTB
+//      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1; break;
+//    default: return;              //    0 to 11 are valid channels on the LM4F120
+//  }
+//  delay = SYSCTL_RCGCGPIO_R;      // 2) allow time for clock to stabilize
+//  delay = SYSCTL_RCGCGPIO_R;
+//  switch(channelNum){
+//    case 0:                       //      Ain0 is on PE3
+//      GPIO_PORTE_DIR_R &= ~0x08;  // 3.0) make PE3 input
+//      GPIO_PORTE_AFSEL_R |= 0x08; // 4.0) enable alternate function on PE3
+//      GPIO_PORTE_DEN_R &= ~0x08;  // 5.0) disable digital I/O on PE3
+//      GPIO_PORTE_AMSEL_R |= 0x08; // 6.0) enable analog functionality on PE3
+//      break;
+//    case 1:                       //      Ain1 is on PE2
+//      GPIO_PORTE_DIR_R &= ~0x04;  // 3.1) make PE2 input
+//      GPIO_PORTE_AFSEL_R |= 0x04; // 4.1) enable alternate function on PE2
+//      GPIO_PORTE_DEN_R &= ~0x04;  // 5.1) disable digital I/O on PE2
+//      GPIO_PORTE_AMSEL_R |= 0x04; // 6.1) enable analog functionality on PE2
+//      break;
+//    case 2:                       //      Ain2 is on PE1
+//      GPIO_PORTE_DIR_R &= ~0x02;  // 3.2) make PE1 input
+//      GPIO_PORTE_AFSEL_R |= 0x02; // 4.2) enable alternate function on PE1
+//      GPIO_PORTE_DEN_R &= ~0x02;  // 5.2) disable digital I/O on PE1
+//      GPIO_PORTE_AMSEL_R |= 0x02; // 6.2) enable analog functionality on PE1
+//      break;
+//    case 3:                       //      Ain3 is on PE0
+//      GPIO_PORTE_DIR_R &= ~0x01;  // 3.3) make PE0 input
+//      GPIO_PORTE_AFSEL_R |= 0x01; // 4.3) enable alternate function on PE0
+//      GPIO_PORTE_DEN_R &= ~0x01;  // 5.3) disable digital I/O on PE0
+//      GPIO_PORTE_AMSEL_R |= 0x01; // 6.3) enable analog functionality on PE0
+//      break;
+//    case 4:                       //      Ain4 is on PD3
+//      GPIO_PORTD_DIR_R &= ~0x08;  // 3.4) make PD3 input
+//      GPIO_PORTD_AFSEL_R |= 0x08; // 4.4) enable alternate function on PD3
+//      GPIO_PORTD_DEN_R &= ~0x08;  // 5.4) disable digital I/O on PD3
+//      GPIO_PORTD_AMSEL_R |= 0x08; // 6.4) enable analog functionality on PD3
+//      break;
+//    case 5:                       //      Ain5 is on PD2
+//      GPIO_PORTD_DIR_R &= ~0x04;  // 3.5) make PD2 input
+//      GPIO_PORTD_AFSEL_R |= 0x04; // 4.5) enable alternate function on PD2
+//      GPIO_PORTD_DEN_R &= ~0x04;  // 5.5) disable digital I/O on PD2
+//      GPIO_PORTD_AMSEL_R |= 0x04; // 6.5) enable analog functionality on PD2
+//      break;
+//    case 6:                       //      Ain6 is on PD1
+//      GPIO_PORTD_DIR_R &= ~0x02;  // 3.6) make PD1 input
+//      GPIO_PORTD_AFSEL_R |= 0x02; // 4.6) enable alternate function on PD1
+//      GPIO_PORTD_DEN_R &= ~0x02;  // 5.6) disable digital I/O on PD1
+//      GPIO_PORTD_AMSEL_R |= 0x02; // 6.6) enable analog functionality on PD1
+//      break;
+//    case 7:                       //      Ain7 is on PD0
+//      GPIO_PORTD_DIR_R &= ~0x01;  // 3.7) make PD0 input
+//      GPIO_PORTD_AFSEL_R |= 0x01; // 4.7) enable alternate function on PD0
+//      GPIO_PORTD_DEN_R &= ~0x01;  // 5.7) disable digital I/O on PD0
+//      GPIO_PORTD_AMSEL_R |= 0x01; // 6.7) enable analog functionality on PD0
+//      break;
+//    case 8:                       //      Ain8 is on PE5
+//      GPIO_PORTE_DIR_R &= ~0x20;  // 3.8) make PE5 input
+//      GPIO_PORTE_AFSEL_R |= 0x20; // 4.8) enable alternate function on PE5
+//      GPIO_PORTE_DEN_R &= ~0x20;  // 5.8) disable digital I/O on PE5
+//      GPIO_PORTE_AMSEL_R |= 0x20; // 6.8) enable analog functionality on PE5
+//      break;
+//    case 9:                       //      Ain9 is on PE4
+//      GPIO_PORTE_DIR_R &= ~0x10;  // 3.9) make PE4 input
+//      GPIO_PORTE_AFSEL_R |= 0x10; // 4.9) enable alternate function on PE4
+//      GPIO_PORTE_DEN_R &= ~0x10;  // 5.9) disable digital I/O on PE4
+//      GPIO_PORTE_AMSEL_R |= 0x10; // 6.9) enable analog functionality on PE4
+//      break;
+//    case 10:                      //       Ain10 is on PB4
+//      GPIO_PORTB_DIR_R &= ~0x10;  // 3.10) make PB4 input
+//      GPIO_PORTB_AFSEL_R |= 0x10; // 4.10) enable alternate function on PB4
+//      GPIO_PORTB_DEN_R &= ~0x10;  // 5.10) disable digital I/O on PB4
+//      GPIO_PORTB_AMSEL_R |= 0x10; // 6.10) enable analog functionality on PB4
+//      break;
+//    case 11:                      //       Ain11 is on PB5
+//      GPIO_PORTB_DIR_R &= ~0x20;  // 3.11) make PB5 input
+//      GPIO_PORTB_AFSEL_R |= 0x20; // 4.11) enable alternate function on PB5
+//      GPIO_PORTB_DEN_R &= ~0x20;  // 5.11) disable digital I/O on PB5
+//      GPIO_PORTB_AMSEL_R |= 0x20; // 6.11) enable analog functionality on PB5
+//      break;
+//  }
+//  DisableInterrupts();
+//  SYSCTL_RCGCADC_R |= 0x01;     // activate ADC0 
+//  SYSCTL_RCGCTIMER_R |= 0x01;   // activate timer0 
+//  delay = SYSCTL_RCGCTIMER_R;   // allow time to finish activating
+//  TIMER0_CTL_R = 0x00000000;    // disable timer0A during setup
+//  TIMER0_CTL_R |= 0x00000020;   // enable timer0A trigger to ADC
+//  TIMER0_CFG_R = 0;             // configure for 32-bit timer mode
+//  TIMER0_TAMR_R = 0x00000002;   // configure for periodic mode, default down-count settings
+//  TIMER0_TAPR_R = 0;            // prescale value for trigger
+//  TIMER0_TAILR_R = period-1;    // start value for trigger
+//  TIMER0_IMR_R = 0x00000000;    // disable all interrupts
+//  TIMER0_CTL_R |= 0x00000001;   // enable timer0A 32-b, periodic, no interrupts
+//  ADC0_PC_R = 0x01;         // configure for 125K samples/sec
+//  ADC0_SSPRI_R = 0x3210;    // sequencer 0 is highest, sequencer 3 is lowest
+//  ADC0_ACTSS_R &= ~0x08;    // disable sample sequencer 3
+//  ADC0_EMUX_R = (ADC0_EMUX_R&0xFFFF0FFF)+0x5000; // timer trigger event
+//  ADC0_SSMUX3_R = channelNum;
+//  ADC0_SSCTL3_R = 0x06;          // set flag and end                       
+//  ADC0_IM_R |= 0x08;             // enable SS3 interrupts
+//  ADC0_ACTSS_R |= 0x08;          // enable sample sequencer 3
+//  NVIC_PRI4_R = (NVIC_PRI4_R&0xFFFF00FF)|0x00004000; //priority 2
+//  NVIC_EN0_R = 1<<17;              // enable interrupt 17 in NVIC
 
-}
+//}
+//Init 1
 void ADC0_InitTimer0ATriggerSeq3PD3(uint32_t period){
   volatile uint32_t delay;
   SYSCTL_RCGCADC_R |= 0x01;     // 1) activate ADC0 
@@ -261,4 +262,89 @@ volatile uint32_t ADCvalue;
 void ADC0Seq3_Handler(void){
   ADC0_ISC_R = 0x08;          // acknowledge ADC sequence 3 completion
   ADCvalue = ADC0_SSFIFO3_R;  // 12-bit result
+}
+
+//init 2
+void ADC0_InitSWTriggerSeq3_Ch9(void){ 
+  SYSCTL_RCGCADC_R |= 0x0001;   // 7) activate ADC0 
+                                  // 1) activate clock for Port E
+  SYSCTL_RCGCGPIO_R |= 0x10;
+  while((SYSCTL_PRGPIO_R&0x10) != 0x10){};
+  GPIO_PORTE_DIR_R &= ~0x10;      // 2) make PE4 input
+  GPIO_PORTE_AFSEL_R |= 0x10;     // 3) enable alternate function on PE4
+  GPIO_PORTE_DEN_R &= ~0x10;      // 4) disable digital I/O on PE4
+  GPIO_PORTE_AMSEL_R |= 0x10;     // 5) enable analog functionality on PE4
+    
+//  while((SYSCTL_PRADC_R&0x0001) != 0x0001){};    // good code, but not yet implemented in simulator
+
+
+  ADC0_PC_R &= ~0xF;              // 7) clear max sample rate field
+  ADC0_PC_R |= 0x1;               //    configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x0123;          // 8) Sequencer 3 is highest priority
+  ADC0_ACTSS_R &= ~0x0008;        // 9) disable sample sequencer 3
+  ADC0_EMUX_R &= ~0xF000;         // 10) seq3 is software trigger
+  ADC0_SSMUX3_R &= ~0x000F;       // 11) clear SS3 field
+  ADC0_SSMUX3_R += 9;             //    set channel
+  ADC0_SSCTL3_R = 0x0006;         // 12) no TS0 D0, yes IE0 END0
+  ADC0_IM_R &= ~0x0008;           // 13) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;         // 14) enable sample sequencer 3
+}
+
+
+//------------ADC0_InSeq3------------
+// Busy-wait Analog to digital conversion
+// Input: none
+// Output: 12-bit result of ADC conversion
+uint32_t ADC0_InSeq3(void){  uint32_t result;
+  ADC0_PSSI_R = 0x0008;            // 1) initiate SS3
+  while((ADC0_RIS_R&0x08)==0){};   // 2) wait for conversion done
+    // if you have an A0-A3 revision number, you need to add an 8 usec wait here
+  result = ADC0_SSFIFO3_R&0xFFF;   // 3) read result
+  ADC0_ISC_R = 0x0008;             // 4) acknowledge completion
+  return result;
+}
+
+//init 3
+void ADC0_InitGPIOTrigger(void){ 
+  SYSCTL_RCGCADC_R |= 0x0001;   // 7) activate ADC0 
+                                  // 1) activate clock for Port E
+  SYSCTL_RCGCGPIO_R |= 0x10;
+  while((SYSCTL_PRGPIO_R&0x10) != 0x10){};
+  GPIO_PORTE_DIR_R &= ~0x10;      // 2) make PE4 input
+  GPIO_PORTE_AFSEL_R |= 0x10;     // 3) enable alternate function on PE4
+  GPIO_PORTE_DEN_R &= ~0x10;      // 4) disable digital I/O on PE4
+  GPIO_PORTE_AMSEL_R |= 0x10;     // 5) enable analog functionality on PE4
+    
+//  while((SYSCTL_PRADC_R&0x0001) != 0x0001){};    // good code, but not yet implemented in simulator
+
+
+  ADC0_PC_R &= ~0xF;              // 7) clear max sample rate field
+  ADC0_PC_R |= 0x1;               //    configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x0123;          // 8) Sequencer 3 is highest priority
+  ADC0_ACTSS_R &= ~0x0008;        // 9) disable sample sequencer 3
+  ADC0_EMUX_R &= 0x4000;         // 10) seq3 is software trigger
+  ADC0_SSMUX3_R &= ~0x000F;       // 11) clear SS3 field
+  ADC0_SSMUX3_R += 9;             //    set channel
+  ADC0_SSCTL3_R = 0x0006;         // 12) no TS0 D0, yes IE0 END0
+  ADC0_IM_R &= ~0x0008;           // 13) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;         // 14) enable sample sequencer 3
+	//init portd for GPIO interrupt	
+	SYSCTL_RCGCGPIO_R |= 0x00000008; // (a) activate clock for port D
+	while ((SYSCTL_RCGCGPIO_R & 0x08) != 0x08){ 		// do nothing
+	}
+  GPIO_PORTD_DIR_R &= ~0x40;    // (c) make PF4 in (built-in button)
+  GPIO_PORTD_AFSEL_R &= ~0x40;  //     disable alt funct on PD4
+  GPIO_PORTD_DEN_R |= 0x40;     //     enable digital I/O on PD4   
+  GPIO_PORTD_AMSEL_R &= ~0x40;       //     disable analog functionality on PF
+  GPIO_PORTD_PDR_R |= 0x40;     //     enable weak pull-up on PF4
+  GPIO_PORTD_IS_R &= ~0x40;     // (d) PF4 is edge-sensitive
+  GPIO_PORTD_IBE_R &= ~0x40;    //     PF4 is not both edges
+  GPIO_PORTD_IEV_R &= ~0x40;    //     PF4 falling edge event
+  GPIO_PORTD_ICR_R = 0x040;      // (e) clear flag4
+  GPIO_PORTD_IM_R |= 0x40;      // (f) arm interrupt on PF4 *** No IME bit as mentioned in Book ***
+  NVIC_PRI0_R = (NVIC_PRI7_R&0x1FFFFFFF)|0x40000000; // (g) priority 
+  NVIC_EN0_R |= 0x08;      // (h) enable interrupt 30 in NVIC
+	GPIO_PORTD_ADCCTL_R |= 0x4;
+	
+  EnableInterrupts();           // (i) Clears the I bit
 }
